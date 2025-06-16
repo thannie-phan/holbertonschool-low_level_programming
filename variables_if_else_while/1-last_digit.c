d87f2806a8880f0cc43137864033d51936e98e49
“thannie-phan” <thannie.phan@gmail.com>
2025-06-16 04:53:19 +0000

    task1

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the size of various types
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int last_digit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	last_digit = n % 10;
+	if (last_digit > 5)
+	printf("Last digit of %d is %e and is greater than 5", n, last_digit)
+	else if (last_digit == 0)
+	printf("Last digit of %d is %e and is 0", n, last_digit)
+	else if (last_digit < 0 && last_digit !0)
+	printf("Last digit of %d is %e and is less than 6 and not 0", n, last_digit)
+	return (0);
+}
