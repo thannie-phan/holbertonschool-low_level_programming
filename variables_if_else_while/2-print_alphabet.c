df527fd026c82b95a339d1ce2b1245ce51b7e423
“thannie-phan” <thannie.phan@gmail.com>
2025-06-16 05:52:12 +0000

    task2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the size of various types
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= : 'z')
+	putchar (c++);
+	putchar ('\n');
+	return (0);
+}
