2583a44a8e1016b2c0313e109a45a56b4a390758
“thannie-phan” <thannie.phan@gmail.com>
2025-06-17 00:41:55 +0000

    task3 2

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -13,10 +13,10 @@ int main(void)
 {
 	char letter;
 
-	for (letter = 'a', letter <= 'z', letter++)
+	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar (letter);
-	for (letter = ''A, letter <= 'Z', letter++)
+	for (letter = 'A'; letter <= 'Z'; letter++)
 		putchar (letter);
-	putchar (’n’);
+	putchar (’\n’);
 	return (0);
 }
