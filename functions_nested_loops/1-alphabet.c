20731ae85f27468e8c56203e0bf224c858feac1e
“thannie-phan” <thannie.phan@gmail.com>
2025-06-17 07:06:28 +0000

    task1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: Fill in on github after
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+{
+	_putchar(letter);
+	letter++;
+}
+return (0);
+}
