2acc36238c96fd18dd11f68644a82e798d4e156d
“thannie-phan” <thannie.phan@gmail.com>
2025-06-17 02:26:00 +0000

    task6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the size of various types
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int number = '48';
+
+	while (number <= '57')
+	putchar (number++);
+	putchar ('\n');
+	return (0);
+}
