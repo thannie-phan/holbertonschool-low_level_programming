fc5b0b36ff83ec0f272f925847ee765485130a28
“thannie-phan” <thannie.phan@gmail.com>
2025-06-17 23:11:55 +0000

    task2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet, lowercase, then new line.
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	for (int i = 0; i < 11; i++)
+
+	print_alphabet();
+}
