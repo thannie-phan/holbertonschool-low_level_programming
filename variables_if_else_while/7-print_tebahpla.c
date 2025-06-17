b1689b66fea25cd5cb191226ae5140e338a91bcf
“thannie-phan” <thannie.phan@gmail.com>
2025-06-17 02:40:25 +0000

    task7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the size of various types
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+	putchar (letter);
+	putchar ('\n');
+	return (0);
+}
