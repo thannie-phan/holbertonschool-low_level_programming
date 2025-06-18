31f65f344227f67a3a8516a5f38cdba7f34e38da
“thannie-phan” <thannie.phan@gmail.com>
2025-06-18 01:11:22 +0000

    task6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - computes absolutes value of an integer or rm - if found
+ * @n: the integer to check
+ * Return: absolutes value of integer
+ *
+ *
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+	return (n * -1);
+	}
+	else
+	{
+	return (n);
+	}
+}
