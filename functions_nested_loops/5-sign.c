05137cc0b7fa576676b748939a0d9163669a908b
“thannie-phan” <thannie.phan@gmail.com>
2025-06-18 00:31:09 +0000

    task5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"i
+
+/**
+ * print_sign - print sign of number
+ * @n: number to check
+ * Return: 1 and print + if n> 0,
+ *	0 and print  0 if n = 0,
+ *	-1 and print - if n < 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+}
