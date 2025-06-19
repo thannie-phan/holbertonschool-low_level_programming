7e9b2453d984e245b5454de8a0ba67ae624c02cf
“thannie-phan” <thannie.phan@gmail.com>
2025-06-19 06:32:07 +0000

    task10 2

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * add(int, int) - adds 2 integers
+ * add - adds 2 integers
  * @a: first integer
  * @b: second integer
  * Return: results of adding the 2 integers
  *
  *
  */
-int add(int a, int b);
+int add(int a, int b)
 {
 
 int sum = a + b;
