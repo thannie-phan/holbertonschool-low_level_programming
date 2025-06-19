334fe01068b8811ed370198c39049874a5487bce
“thannie-phan” <thannie.phan@gmail.com>
2025-06-19 06:55:48 +0000

    task11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ * print_to_98 - print all numbers to 98
+ * @n: number to check
+ *
+ *
+ */
+void print_to_98(int n);
+{
+	if (n == 98)
+	{
+	printf("%d\n", n);
+	return;
+	}
+	else if (n < 98)
+	{
+	printf("%d, ", n);
+	n++
+	}
+	else
+	{
+	printf("%d, ", n);
+	n--
+	}
+}
