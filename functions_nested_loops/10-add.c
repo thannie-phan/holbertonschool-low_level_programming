431cc3432f99c86c386d03a53c0cac18c4107298
“thannie-phan” <thannie.phan@gmail.com>
2025-06-19 06:29:33 +0000

    task 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add(int, int) - adds 2 integers
+ * @a: first integer
+ * @b: second integer
+ * Return: results of adding the 2 integers
+ *
+ *
+ */
+int add(int a, int b);
+{
+
+int sum = a + b;
+
+return (sum);
+}
