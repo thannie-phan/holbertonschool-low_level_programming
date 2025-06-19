f464dc338e57b3ad71132639f725ef755af1b788
“thannie-phan” <thannie.phan@gmail.com>
2025-06-19 05:57:58 +0000

    task9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * time_table - print the 9x table
+ * starting with 0
+ */
+void time_table(void)
+{
+int i, j, k;
+
+for (i = 0; i < 8; i++)
+{
+	for (j = 0; j < 8; j++)
+	{
+		k = i * j;
+		if (j == 0)
+		{
+			_putchar(k + '0');
+		}
+
+		if (k < 10 && j != 0)
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+			_putchar(k + '0');
+		}
+		else if (k <= 10)
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar((k / 10) + '0');
+			_putchar((k % 10) + '0');
+		}
+	}
+	_putchar('\n/);
+}
+}
