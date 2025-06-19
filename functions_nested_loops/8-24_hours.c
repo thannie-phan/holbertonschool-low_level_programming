9230d11aa6b57ea5d1cd02ae40aaa271c4b5aff0
“thannie-phan” <thannie.phan@gmail.com>
2025-06-19 00:19:38 +0000

    task8 2

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -13,10 +13,10 @@ for (i = 0; i < 24; i++)
 	for (j = 0; j < 60; j++)
 	{
 	_putchar('0' + i / 10);
-	_putchar('0' + i / 10);
+	_putchar('0' + i % 10);
 	_putchar(':');
 	_putchar('0' + j / 10);
-	_putchar('0' + j / 10);
+	_putchar('0' + j % 10);
 	_putchar('\n');
 	}
 }
