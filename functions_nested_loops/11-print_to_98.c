6c7fbe8cdbab7a3b398db32cb39f60eec6e4ddd1
“thannie-phan” <thannie.phan@gmail.com>
2025-06-19 07:06:07 +0000

    task11 4

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -9,19 +9,13 @@
  */
 void print_to_98(int n)
 {
-	if (n == 98)
+	while (n != 98)
 	{
-	printf("%d\n", n);
-	return;
-	}
-	else if (n < 98)
-	{
-	printf("%d, ", n);
+	printf("%d", ", n);
+	if (n < 98)
 	n++;
-	}
 	else
-	{
-	printf("%d, ", n);
 	n--;
 	}
+	printf ("d\n, 98);
 }
