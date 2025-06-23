9fec5719a49f04da2f835b83a4fbe64f9f1c580a
“thannie-phan” <thannie.phan@gmail.com>
2025-06-23 02:03:36 +0000

    task 5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 10x the numbers, from 0-14 then new line.
+ *
+ * Return: Always 0 (Success)
+ */
+void more_numbers(void)
+{
+	int i;
+	int n;
+
+	for (i = 0; i < 10; i++)
+{
+	for (n = 0; c <= 14; n++)
+	{
+	if (n >= 10)
+	_putchar('0' + n / 10);
+	_putchar('0' + n % 10);
+	}
+	_putchar('\n');
+}
+}
