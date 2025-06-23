8d9992479fc2a4a08c094d6cd6f0bb664c95bf2d
“thannie-phan” <thannie.phan@gmail.com>
2025-06-23 04:55:11 +0000

    task7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print a diagonal line in the terminal
+ * @n: the number of times the character \ should be printed
+ * Return: Always 0 (Success)
+ */
+void print_diagonal(int n)
+{
+	int current_line = 0;
+
+	if (number_of_lines <= 0)
+	{
+	_putchar('\n');
+	return;
+	}
+
+	while (current_line < number_of_lines)
+	{
+	int number_of_spaces = 0;
+
+	while (number_of_spaces < current_line)
+	{
+	_putchar(' ');
+	number_of_spaces++;
+	}
+	_putchar('\\')
+	_putchar('\n');
+	current_line++;
+}
