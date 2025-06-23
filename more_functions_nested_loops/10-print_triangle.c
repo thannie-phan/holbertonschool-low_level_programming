c783dfa7cf7e1fe5560a467299f6105314362a05
“thannie-phan” <thannie.phan@gmail.com>
2025-06-23 06:35:15 +0000

    task10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_triangle - print a right aligned triangle followed by new line
+ * @triangle_size: this is the size of triangle
+ * Return: Always 0 (Success)
+ */
+void print_triangle(int triangle_size)
+{
+	int current_row = 1;
+
+	if (triangle_size <= 0)
+	{
+	_putchar('\n');
+
+	return;
+	}
+	while (current_row < triangle_size)
+	{
+	int number_of_spaces = triangle_size - current_row;
+	int number_of_hashes = current_row;
+
+	while (number_of_spaces > 0)
+	{
+	_putchar(' ');
+	number_of_spaces--;
+	}
+	while (number_of_hashes > 0)
+	{
+	_putchar('#');
+	number_of_hashes--;
+	}
+	_putchar('\n');
+	current_row++;
+	}
+}
