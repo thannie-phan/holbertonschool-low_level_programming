8e1cc0f5177be30c07eada067dcd6ae1922cc16e
“thannie-phan” <thannie.phan@gmail.com>
2025-06-23 01:35:30 +0000

    task2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiply 2 integers
+ * @a: first integer
+ * @b: second integer
+ * Return: results of multiplying the 2 integers
+ *
+ *
+ */
+int mul(int a, int b)
+{
+
+int sum = a * b;
+
+return (sum);
+}
