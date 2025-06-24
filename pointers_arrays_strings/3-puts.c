e3e43d55860dfe99837f868b78b3b7241fb12e17
“thannie-phan” <thannie.phan@gmail.com>
2025-06-24 04:08:24 +0000

    task3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - print a string, then new line
+ * @str: the string
+ * Return: (0)
+ */
+int _puts(char *str)
+{
+	int length_of_string = 0;
+
+	while (str[length_of_string] != '\0')
+	{
+	_putchar(str[length_of_string]);
+	length_of_string++;
+	}
+
+	_putchar('\n')
+}
