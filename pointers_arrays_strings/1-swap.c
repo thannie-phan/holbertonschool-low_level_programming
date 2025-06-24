2e55d5b689484d9a8b7f8e0e496a42678e6c5a68
“thannie-phan” <thannie.phan@gmail.com>
2025-06-24 02:10:43 +0000

    task1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps value of 2 integers
+ * @a: the first integer
+ * @b: the second integer
+ * Return: Always 0 (Success)
+ */
+void swap_int(int *a, int *b)
+{
+	int = temporary_variable;
+	temporary_variable = *a;
+	*a = *b;
+	*b = a;
+}
