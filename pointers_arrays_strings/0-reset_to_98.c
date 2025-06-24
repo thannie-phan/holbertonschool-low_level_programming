e64949b088d3ba1e275f9bcc632cfb3633e6897a
“thannie-phan” <thannie.phan@gmail.com>
2025-06-24 01:56:10 +0000

    task0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int, update value to 98
+ * @n:the variable to check and update
+ * Return: Always 0 (Success)
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is the folder for all the exercises on pointers ararys and strings project
