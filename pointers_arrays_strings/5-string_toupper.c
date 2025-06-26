7d602ae9a03f45cc45d95c7913bb8961740361c0
“thannie-phan” <thannie.phan@gmail.com>
2025-06-26 07:32:12 +0000

    task5

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ * string_toupper - changes all lowercase to uppercase
+ * @str: the pointer
+ * Return: str
+ */
+char *string_toupper(char *str)
+{
+	int string;
+
+	while (str[string] != '\0')
+	{
+	if (string >= 'a' && string <= 'z')
+	{
+	str(string) -= 32;
+	}
+	string++;
+	}
+	return (str);
+}
