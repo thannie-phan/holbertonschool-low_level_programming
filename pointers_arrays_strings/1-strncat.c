5d8aefb9c646a3637fd15f3bfdb402853b8c9ca6
“thannie-phan” <thannie.phan@gmail.com>
2025-06-26 04:35:56 +0000

    task0

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -10,14 +10,14 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int n = 0;
+	int first_string = 0;
 	int sec_string = 0;
 
-	while (src[sec_string] != '\0')
+	while (dest[sec_string] != '\0')
 	{
 	sec_string++;
 	}
-	while (src[n] != ;\0')
+	while (src[first_string] != '\0' && first_string < n)
 	{
 	dest[sec_string] = src[n];
 	n++;
