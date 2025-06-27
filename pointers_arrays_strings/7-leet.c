fe4653b1040079cf3714fa7fc0e069cf17b8e977
“thannie-phan” <thannie.phan@gmail.com>
2025-06-27 00:06:01 +0000

    task7

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @str: the pointer
+ * Return: 0
+ */
+char *leet(char *str)
+{
+	char lett[] = "aAeEoOtTlL";
+	char num[] = "4433007711";
+	int sen = 0;
+	int check;
+
+	while (str[sen] != '\0')
+	{
+	check = 0;
+	while (lett[check] != '\0')
+	{
+	if (str[sen] == lett[check]
+	{
+	str[sen] = num[check];
+	}
+	check++;
+	}
+	sen++
+	}
+	return (str);
+}
