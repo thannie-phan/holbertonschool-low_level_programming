e8b275f059b4458cc059c88fca17e8a13b106715
“thannie-phan” <thannie.phan@gmail.com>
2025-06-30 07:09:25 +0000

    task 3

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include "stddef.h"
+
+/**
+ * _strspn - gets length of a prefix substring
+ * @s: string string
+ * @accept: the substring
+ * Return: number of bytes in s that consist of bytes from accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count = 0
+
+	int look
+	int match;
+
+	while (*s != '\0')
+	{
+	look = 0;
+	match = 0;
+	while (accept[look] != '\0')
+	{
+	if (*s == accept[look])
+	{
+	match = 1;
+	break;
+	}
+	look++;
+	}
+	if (match == 0)
+	{
+	break;
+	}
+	count++;
+	s++
+	}
+	return (count);
+}
