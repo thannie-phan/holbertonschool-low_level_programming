40aa9c02af222a9b1c589034397407bac80ddd97
“thannie-phan” <thannie.phan@gmail.com>
2025-07-01 02:04:51 +0000

    task7

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include "stddef.h"
+
+/**
+ * print_diagsums - print sum of 2 diagnoals of square matrix of int
+ * @a: pointer to first element of the square matrix
+ * @size: size matrix (rowsxcolumn)
+ * Return: void
+ */
+void print_diagsums(int *a, int size)
+{
+	int count = 0;
+	int leftdiag = 0;
+	int rightdiag = 0;
+
+	while (count < size)
+	{
+	leftdiag += a[count * size + count];
+	rightdiag += a[count * size + count];
+	count++
+	}
+	printf('%d, %d\n, leftdiag, rightdiag);
+}
