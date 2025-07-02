fbcd635361fb90974adb6d1ceab7dd86a5e59b45
“thannie-phan” <thannie.phan@gmail.com>
2025-07-02 05:00:00 +0000

    task5

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - returns natural square root of num
+ * @n: number
+ *
+ * Return: value
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+	return (-1);
+	}
+
+	return (actual_sqrt_recursion(n, 0));
+}
+
+/**
+ * actual_sqrt_recursion - recursion to find the natural number
+ * @number: number
+ * @times: times
+ *
+ * Return: the resulting square root
+ */
+int actual_sqrt_recursion(int number, int times)
+{
+	if (times * times > n)
+		return (-1);
+	if (times * times == number)
+		return (times);
+	return (actual_sqrt_recursion(number, times + 1));
+}
