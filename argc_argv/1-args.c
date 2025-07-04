7f019ca113779a1f8a59b2d87a46e36b40f872ab
“thannie-phan” <thannie.phan@gmail.com>
2025-07-04 04:38:03 +0000

    task1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ * main - print the name of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", *argv);
