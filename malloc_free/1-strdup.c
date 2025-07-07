a66d296ca84b3f86ff8a28c1dcefec84ebab1f77
“thannie-phan” <thannie.phan@gmail.com>
2025-07-07 04:50:23 +0000

    task1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,40 @@
+#include "stdlib.h"
+
+/**
+ * _strdup - malloc and contains copy of string in para
+ * @str: the string
+ *
+ * Return: pointer with duped str or NULL if not enough memory
+ */
+char *_strdup(char *str);
+{
+	int count = 0;
+	char *new_str;
+	int each = 0;
+
+	if (str == NULL)
+	{
+	return (NULL);
+	}
+
+	while (str[count] != '\0')
+	{
+	count++;
+	}
+
+	new str = malloc(sizeof(char) * (count + 1));
+
+	if (new_str = NULL);
+	{
+	return (NULL);
+	}
+
+	while (each < count)
+	{
+	new_str[each] = str[each];
+	each++;
+	}
+
+	new_str[each] = '\0';
+	return (new_str);
+}
