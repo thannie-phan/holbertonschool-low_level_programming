7d6c453f4a503ee0c2f09fdf5570dd90b94dbcdc
“thannie-phan” <thannie.phan@gmail.com>
2025-07-07 03:40:43 +0000

    task0

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ * create_array - create an array of chars and initalizes it
+ * @size: size of arrays
+ * @c: the char to intialises the array with
+ * Return: NULL if size = 0 or fail
+ */
+char *create_array(unsigned int size, char c);
+{
+	char *array;
+	unsigned int find = 0;
+
+	if (size == 0)
+	{
+	return (NULL);
+	}
+
+	array = malloc(sizeof(char) * size);
+	if (array == NULL)
+	{
+	return (NULL);
+	}
+
+	while (find < size)
+	{
+	array[find] = c;
+	find++;
+	}
+
+	return (array);
+
+}
