5d036819a8f2dcd59b4718b8860a85745695926c
“thannie-phan” <thannie.phan@gmail.com>
2025-07-08 00:06:40 +0000

    task4

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,24 @@
+#include "stdlib.h"
+
+/**
+ * free_grid - frees a 2D grid prev created by alloc_grid
+ * @grid: address location of 2D grid
+ * @height: no of rows in grid
+ *
+ * Return:
+ */
+void free_grid(int **grid, int height)
+{
+	int row = 0;
+
+	if (grid == NULL || height <= 0)
+	return;
+
+	while (row < height)
+	{
+	row++;
+	free(grid[row]);
+	}
+
+	free(grid);
+}
