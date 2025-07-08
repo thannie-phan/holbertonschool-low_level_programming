a32e91a3033068eeadd8c7c4dfa3e51099de1420
“thannie-phan” <thannie.phan@gmail.com>
2025-07-08 23:41:16 +0000

    task 0

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "stdlib.h"
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: what to allocate memory for
+ *
+ * Return: void. if fails, exit (98)
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *pointer;
+
+	pointer = malloc(b);
+
+	if (pointer == NULL)
+	exit(98);
+
+	return (pointer);
+}
