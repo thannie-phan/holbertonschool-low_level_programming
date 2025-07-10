3801a1cb194014f5a4b56254c287055fea26c2ba
“thannie-phan” <thannie.phan@gmail.com>
2025-07-10 05:25:25 +0000

    task1

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "stdlib.h"
+#include "main.h"
+
+/**
+ * init_dog - initialise variable of type struct dog
+ * @d: pointer to dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+	return;
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
