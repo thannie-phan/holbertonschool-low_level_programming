fe4528ae4b71431f056c0bd331695fed7d2b9fc5
“thannie-phan” <thannie.phan@gmail.com>
2025-07-10 07:46:26 +0000

    task5

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include "stdlib.h"
+#include "main.h"
+#include "dog.h"
+
+/**
+ * free_dog - frees dog function
+ * @d: pointer to dog_t
+ *
+ *
+ *
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
