014166658171d7adebf7cd2c6ecda1c6636c3c19
“thannie-phan” <thannie.phan@gmail.com>
2025-07-10 05:08:07 +0000

    task0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct for dog data
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
