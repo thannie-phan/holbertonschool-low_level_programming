70f6bea22e4ce970caab33ba4cb42ce1ba6d8fce
“thannie-phan” <thannie.phan@gmail.com>
2025-07-15 05:04:07 +0000

    task2

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -18,7 +18,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	while (count < n)
 	{
-	string = va_arg(args, char *)
+	string = va_arg(args, char *);
 	if (string == NULL)
 	{
 	printf("(nil)");
