e6c352668ae650082a97088c158b06d5870c93d7
“thannie-phan” <thannie.phan@gmail.com>
2025-07-15 01:16:38 +0000

    task3

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -16,28 +16,28 @@ int main(int argc, char *argv[])
 	int result;
 	int a, b;
 	char *s;
-	int (*f) (int, int);
+	int (*f)(int, int);
 
 	if (argc != 4)
 	{
 	printf("Error\n");
-	exit (98);
+	exit(98);
 	}
 
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
-	s = argv[2]; 
+	s = argv[2];
 
 	if (get_op_func(s) == NULL)
 	{
 	printf("Error\n");
-	exit (99);
+	exit(99);
 	}
 
 	if ((strcmp(s, "/") == 0 || strcmp(s, "%") == 0) && (b == 0))
 	{
 	printf("Error\n");
-	exit (100);
+	exit(100);
 	}
 
 	f = get_op_func(s);
