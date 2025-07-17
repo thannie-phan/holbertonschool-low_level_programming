cd351bdd422d11b30a979e8a8a6a5dd32d145be7
“thannie-phan” <thannie.phan@gmail.com>
2025-07-17 04:39:51 +0000

    task3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -2,12 +2,6 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-void print_char(va_list arg);
-void print_int(va_list arg);
-void print_float(va_list arg);
-void print_string(va_list arg);
-void print_all(const char * const format, ...);
-
 /**
  * print_char - print char
  * @arg: va_list with the char to print
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,11 +1,17 @@
 #ifndef variadic_functions_h
 #define variadic_functions_h
+
 #include <stdarg.h>
 
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+
 /**
- * struct printer - Struct to match  format symbol with a print function
- * @symbol: symbol 'c', 'i', 'f', 's'
- * @print: Pointer to function that print
+ * struct printer - Struct printer
+ *
+ * @symbol: The symbol shorten
+ * @print: The function
  */
 struct printer
 {
@@ -14,9 +20,10 @@ struct printer
 
 };
 
-int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
+void print_char(va_list arg);
+void print_int(va_list arg);
+void print_float(va_list arg);
+void print_string(va_list arg);
 void print_all(const char * const format, ...);
 
 #endif
