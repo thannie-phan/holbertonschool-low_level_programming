ed6c22fcacb50909595a6417b851451faa5a996a
“thannie-phan” <thannie.phan@gmail.com>
2025-07-17 03:47:57 +0000

    task3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,6 +1,67 @@
-#include <stdarg.h>
 #include "variadic_functions.h"
 #include <stdio.h>
+#include <stdarg.h>
+
+void print_char(va_list arg);
+void print_int(va_list arg);
+void print_float(va_list arg);
+void print_string(va_list arg);
+void print_all(const char * const format, ...);
+
+/**
+ * print_char - print char
+ * @arg: va_list with the char to print
+ */
+void print_char(va_list arg)
+{
+	char letter;
+
+	letter = va_arg(arg, int);
+	printf("%c", letter);
+}
+
+/**
+ * print_int - print int
+ * @arg: va_list with the int to print
+ */
+void print_int(va_list arg)
+{
+	int num;
+
+	num = va_arg(arg, int);
+	printf("%d", num);
+}
+
+/**
+ * print_float - printlfloat
+ * @arg: va_list with the float to print
+ */
+void print_float(va_list arg)
+{
+	float num;
+
+	num = va_arg(arg, double);
+	printf("%f", num);
+}
+
+/**
+ * print_string - print string
+ * @arg: va_list with the string to print
+ */
+void print_string(va_list arg)
+{
+	char *string;
+
+	string = va_arg(arg, char *);
+
+	if (string == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+
+	printf("%s", string);
+}
 
 /**
  * print_all- prints everything
@@ -11,36 +72,39 @@
 void print_all(const char * const format, ...)
 {
 	va_list args;
-	int count = 0;
+	int f_count = 0;
+	int p_count;
 	char *separator = "";
-	char *string;
+
+	struct printer printer_format[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string}
+	};
 
 	va_start(args, format);
 
-	while (format && format[count] != '\0')
+	while (format != NULL && format[f_count] != '\0')
 	{
-	if (format[count] == 's' || format[count] == 'i' ||
-	format[count] == 'f' || format[count] == 'c')
-	{
-	printf("%s", separator);
-	if (format[count] == 'c')
-	printf("%c", (char)va_arg(args, int));
-	else if (format[count] == 'i')
-	printf("%d", va_arg(args, int));
-	else if (format[count] == 'f')
-	printf("%f", (float)va_arg(args, double));
-	else if (format[count] == 's')
+		p_count = 0;
+
+		while (p_count < 4 && format[f_count] != *printer_format[p_count].symbol)
 		{
-		string = va_arg(args, char *);
-		if (string == NULL)
-		printf("(nil)");
-		else
-		printf("%s", string);
+			p_count++;
 		}
-	separator = ", ";
+
+		if (p_count < 4)
+		{
+			printf("%s", separator);
+			printer_format[p_count].print(args);
+			separator = ", ";
+		}
+
+		f_count++;
 	}
-	count++;
-	}
-	va_end(args);
+
 	printf("\n");
+	va_end(args);
 }
+
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,6 +1,18 @@
 #ifndef variadic_functions_h
 #define variadic_functions_h
 
+/**
+ * struct printer - Struct to match  format symbol with a print function
+ * @symbol: symbol 'c', 'i', 'f', 's'
+ * @print: Pointer to function that print
+ */
+struct printer
+{
+	char *symbol;
+	void (*print)(va_list arg);
+
+};
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
