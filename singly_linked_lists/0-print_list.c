b67f56852d0ea7afa6a89fd73b7c965452062884
“thannie-phan” <thannie.phan@gmail.com>
2025-07-28 06:44:07 +0000

    task0

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - print out list and return no of nodes
+ * list_t: the data
+ * h: the connector
+ * Return: no of nodes
+ */
+
+size_t print_list(const list_t *h);
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		printf("[0] (nil)\n");
+		else
+		printf("[%u] [%s]\n, h->len, h->str");
+
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
