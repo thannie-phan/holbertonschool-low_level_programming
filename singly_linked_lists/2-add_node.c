e10c7e184a4234c7c0a11e233d2e301db5bf0f33
“thannie-phan” <thannie.phan@gmail.com>
2025-07-28 23:23:56 +0000

    task2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -6,7 +6,7 @@
 
 /**
  * add_node - add new node at start
- * @*h: pointer to head pointer
+ * @head: pointer to head pointer
  * @str: string element to store in node
  *
  * Return: address of new node
@@ -14,12 +14,17 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node = malloc(sizeof(list_t));
+	int length_of_string = 0;
 
 	if (new_node == NULL)
 	return (NULL);
 
 	new_node->str = strdup(str);
-	new_node->len = strlen(new_node->str);
+
+	while (str[length_of_string] != '\0')
+	length_of_string++;
+	new_node->len = length_of_string;
+
 	new_node->next = *head;
 
 	*head = new_node;
