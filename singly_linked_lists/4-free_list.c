38f27e01e523548f13dd0670d8a5df9650c42ab0
“thannie-phan” <thannie.phan@gmail.com>
2025-07-29 01:29:45 +0000

    task4

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <stddef.h>
+#include "lists.h"
+
+/**
+ * free_list - free memory of list_t list
+ * 
+ *
+ *
+ * Return: void
+ */
+void free_list(list_t *headlist)
+{
+	list_t *temp;
+
+	while (heard != NULL)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
