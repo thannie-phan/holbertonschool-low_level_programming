c7b3d2fc2e672b8e72bcec720e67771c4b4cfae1
“thannie-phan” <thannie.phan@gmail.com>
2025-07-29 00:13:36 +0000

    task3

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -5,16 +5,17 @@
 #include "lists.h"
 
 /**
- * add_node - add new node at start
- * @head: pointer to head pointer
+ * add_node_end - add new node at end
+ * @head: double pointer to head pointer
  * @str: string element to store in node
  *
  * Return: address of new node
  */
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node = malloc(sizeof(list_t));
 	int length_of_string = 0;
+	list_t *temp = *head;
 
 	if (new_node == NULL)
 	return (NULL);
@@ -25,9 +26,19 @@ list_t *add_node(list_t **head, const char *str)
 	length_of_string++;
 	new_node->len = length_of_string;
 
-	new_node->next = *head;
+	new_node->next = NULL;
 
-	*head = new_node;
+	if (*head == NULL)
+	{
+		*head = new_node_end;
+		return (new_node_end);
+	}
+
+	while (temp->next != NULL)
+		temp = temp->next;
+
+	temp->next = new_node_end;
 
 	return (new_node);
 }
+
