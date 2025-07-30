d546693a741dd76c24b78a4f54507a168190c1fc
“thannie-phan” <thannie.phan@gmail.com>
2025-07-30 04:26:06 +0000

    task2

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -14,7 +14,6 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
-	 int length_of_string = 0;
 
 	if (new_node == NULL)
 	return (NULL);
@@ -24,7 +23,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new_node->prev = NULL;
 
 	if (*head != NULL)
-	head->prev = new_node;
+	(*head)->prev = new_node;
 
 	*head = new_node;
 
