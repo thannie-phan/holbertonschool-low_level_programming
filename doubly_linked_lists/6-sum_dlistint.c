994b3ea8a6eaec71d6030bf83c7115708ed2675c
“thannie-phan” <thannie.phan@gmail.com>
2025-07-30 04:51:01 +0000

    task6

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * sum_dlistint - sum of all the data (n)
+ * @head: the connector
+ *
+ * Return: sum of all the data of linkedlist. if empty, return 0
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	if (head == NULL)
+	return (0);
+
+	int sum = 0;
+	dlistint_t *point_to_node = head;
+
+	while (point_to_node != NULL)
+	{
+		sum = sum + point_to_node->n;
+		point_to_node = point_to_node->next;
+	}
+	return (sum);
+}
