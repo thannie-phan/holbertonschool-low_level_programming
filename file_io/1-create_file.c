08bf5812c809af071a58f2a6890fc37646bcdcdc
“thannie-phan” <thannie.phan@gmail.com>
2025-08-01 00:58:02 +0000

    task1

---
+++ file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * read_textfile - read the text file
+ * @filename: name of file
+ * @text_content: content to put in file
+ *
+ * Return: 1 if success or -1 if fail
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, write_bytes = 0;
+
+	if (filename == NULL)
+	return (-1)
+
+	if (text_content == NULL)
+		text_content = "";
+
+	while (text_content[write_bytes] != '\0')
+		write_bytes++;
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+	return (-1);
+
+	write(fd, text_content, write_bytes);
+	close(fd);
+	return (1);
+}
