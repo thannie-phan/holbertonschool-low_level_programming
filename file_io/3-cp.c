c516ccabc3d1f8b9b7db7bf62a0b972310a63255
“thannie-phan” <thannie.phan@gmail.com>
2025-08-01 01:24:27 +0000

    task3

---
+++ file_io/3-cp.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include "main.h"
+
+/**
+* main - program that copies the content of a file to another file
+* @argc: num argument
+* @argv: string argument
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int source, dest;
+	int num1 = 1024, num2 = 0;
+	char store_content[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	source = open(argv[1], O_RDONLY);
+	dest = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	if (source == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	while (num1 == 1024)
+	{
+		num1 = read(source, store_content, 1024);
+		if (num1 == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+		num2 = write(dest, store_content, num1);
+		if (num2 < num1)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	}
+	if (close(source) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", source, exit(100);
+	}
+	if (close(dest) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", dest, exit(100);
+	}
+	return (0);
+}
