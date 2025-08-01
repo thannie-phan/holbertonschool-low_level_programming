6fec94d5a74218d7b6daf3593e8db698e9ab0d9d
“thannie-phan” <thannie.phan@gmail.com>
2025-08-01 01:40:06 +0000

    task3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -40,7 +40,7 @@ int main(int argc, char *argv[])
 		num2 = write(dest, store_content, num1);
 		if (num2 < num1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2])
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
 	}
