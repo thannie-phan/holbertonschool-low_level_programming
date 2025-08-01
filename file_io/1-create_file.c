c06724cc4887ff808b43894953ea25b05d628c1b
“thannie-phan” <thannie.phan@gmail.com>
2025-08-01 00:58:59 +0000

    task1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	int fd, write_bytes = 0;
 
 	if (filename == NULL)
-	return (-1)
+	return (-1);
 
 	if (text_content == NULL)
 		text_content = "";
