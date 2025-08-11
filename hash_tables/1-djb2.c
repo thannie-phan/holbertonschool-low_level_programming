fcd9e27e725ef9e5394e42e604a4b9edc19a8f87
“thannie-phan” <thannie.phan@gmail.com>
2025-08-11 01:25:38 +0000

    task1

---
+++ hash_tables/1-djb2.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - hash function implementing the djb2 algorithm.
+ * @str: input str to turn into hash
+ *
+ * Return: hash
+ */
+
+unsigned long int hash_djb2(const unsigned char *str)
+{
+    unsigned long int hash;
+    int c;
+
+    hash = 5381;
+    while ((c = *str++))
+    {
+        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+    }
+    return hash;
+}
