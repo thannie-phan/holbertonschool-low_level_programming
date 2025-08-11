06d9473ab2392ce4ef996e10d0879cb7f4737ef6
“thannie-phan” <thannie.phan@gmail.com>
2025-08-11 01:37:21 +0000

    task2

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -4,29 +4,10 @@
 #include "hash_tables.h"
 
 /**
- * hash_djb2 - hash function implementing the djb2 algorithm.
- * @str: input str to turn into hash
- *
- * Return: hash
- */
-
-unsigned long int hash_djb2(const unsigned char *str)
-{
-	unsigned long int hash;
-	int c;
-
-	hash = 5381;
-	while ((c = *str++))
-	{
-		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-	}
-	return (hash);
-}
-
-/**
  * key_index - give index of the key
  * @key: the key
  * @size: size of array of hash table
+ *
  * Return: int where key is stored
  */
 
