b4525ee3a7c66ddec270c443860001ef665deeaa
“thannie-phan” <thannie.phan@gmail.com>
2025-08-11 01:11:16 +0000

    task0

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -18,7 +18,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (size == 0)
 	return (NULL);
 
-	new_table = malloc(sizeof(hash_node_table));
+	new_table = malloc(sizeof(hash_table_t));
 
 	if (new_table == NULL)
 	return (NULL);
